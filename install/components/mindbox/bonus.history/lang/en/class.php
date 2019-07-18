0abe878f97ee43e6f8574a3962e177bb78eb96fe
Oleg Podchasov <opodchasov@qsoft.ru>
2019-07-18 15:49:31 +0300

    удалены лишние константы в ланговых файлах

--- install/components/mindbox/bonus.history/lang/en/class.php
+++ install/components/mindbox/bonus.history/lang/en/class.php
@@ -2,4 +2,3 @@
 $MESS['MB_BH_MODULE_NOT_INCLUDED'] = 'Error: module #MODULE# not connected';
 $MESS['MB_BH_ERROR_MESSAGE'] = 'Bonus history is empty';
 $MESS['MB_BH_BALANCE_HTML'] = '<tr><td>#START#</td><td>#SIZE#</td><td>#NAME#</td><td>#END#</td></tr>';
-$MESS['MB_BH_BAD_MODULE_SETTING'] = 'Invalid module settings';
--- install/components/mindbox/bonus.history/lang/ru/class.php
+++ install/components/mindbox/bonus.history/lang/ru/class.php
@@ -2,4 +2,3 @@
 $MESS['MB_BH_MODULE_NOT_INCLUDED'] = 'Ошибка: модуль #MODULE# не подключен';
 $MESS['MB_BH_ERROR_MESSAGE'] = 'История бонусов пуста';
 $MESS['MB_BH_BALANCE_HTML'] = '<tr><td>#START#</td><td>#SIZE#</td><td>#NAME#</td><td>#END#</td></tr>';
-$MESS['MB_BH_BAD_MODULE_SETTING'] = 'Ошибка в настройках модуля;
