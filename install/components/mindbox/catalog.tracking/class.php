011448e84312e47a7493eef433544eff0b874e7a
maxim.b <maxim.b@intensa.ru>
2021-02-03 08:21:37 +0300

    rework component mindbox:catalog.tracking

--- install/components/mindbox/catalog.tracking/class.php
+++ install/components/mindbox/catalog.tracking/class.php
@@ -17,15 +17,8 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
     die();
 }
 
-class CatalogTracking extends CBitrixComponent implements Controllerable
+class CatalogTracking extends CBitrixComponent
 {
-    private $actions = [
-        'viewProduct',
-        'viewCategory'
-    ];
-
-    private $mindbox;
-
     public function __construct(CBitrixComponent $component = null)
     {
         parent::__construct($component);
@@ -33,82 +26,12 @@ class CatalogTracking extends CBitrixComponent implements Controllerable
         if (!$this->loadModule()) {
             return;
         }
-
-        $this->mindbox = Options::getConfig();
-    }
-
-    public function configureActions()
-    {
-        return Ajax::configureActions($this->actions);
     }
 
     public function executeComponent()
     {
         $this->includeComponentTemplate();
     }
-
-    public function viewCategoryAction($id)
-    {
-        if (!$this->mindbox) {
-            return Ajax::errorResponse(GetMessage('MB_CT_BAD_MODULE_SETTING'));
-        }
-        $ids = [
-            Options::getModuleOption('EXTERNAL_SYSTEM') => $id
-        ];
-
-        $dto = new DTO([
-            'viewProductCategory' => [
-                'productCategory' => [
-                    'ids' => $ids
-                ]
-            ]
-        ]);
-
-        try {
-            $this->mindbox->getClientV3()
-                ->prepareRequest('POST', Options::getOperationName('viewCategory'), $dto, '', [], false)
-                ->sendRequest();
-        } catch (MindboxClientException $e) {
-            $request = $this->mindbox->getClientV3()->getRequest();
-            if ($request) {
-                QueueTable::push($request);
-            }
-        }
-    }
-
-    public function viewProductAction($id)
-    {
-        if (!$this->mindbox) {
-            return Ajax::errorResponse(GetMessage('MB_CT_BAD_MODULE_SETTING'));
-        }
-        $data = [
-            'viewProduct' => [
-                'product' => []
-            ]
-        ];
-
-        $ids = [
-            Options::getModuleOption('EXTERNAL_SYSTEM') => $id
-        ];
-
-
-        $data['viewProduct']['product'] = ['ids' => $ids];
-
-
-        $dto = new DTO($data);
-
-        try {
-            $this->mindbox->getClientV3()
-                ->prepareRequest('POST', Options::getOperationName('viewProduct'), $dto, '', [], false)
-                ->sendRequest();
-        } catch (MindboxClientException $e) {
-            $request = $this->mindbox->getClientV3()->getRequest();
-            if ($request) {
-                QueueTable::push($request);
-            }
-        }
-    }
-
 
     private function loadModule()
     {
