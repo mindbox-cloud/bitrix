ae47711dcdde5a6ec8255eddac12cd73d87bc1d0
Ilya <i.yablonsky@intensa.ru>
2021-02-10 11:24:53 +0300

    feed hotfix

--- install/version.php
+++ install/version.php
@@ -5,7 +5,7 @@
 
 $arModuleVersion =
 	[
-		"VERSION" => "2.1.2",
+		"VERSION" => "2.1.3",
 		"VERSION_DATE" => "2021-02-04 16:08:00",
 		"MODULE_NAME" => GetMessage("MODULE_NAME"),
 		"MODULE_DESCRIPTION" => GetMessage("MODULE_DESCRIPTION")
