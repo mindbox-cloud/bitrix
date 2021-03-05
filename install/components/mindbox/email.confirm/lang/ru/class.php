a6b3b1440934d7ace2b8167d343699941e71f9c8
maxim.b <maxim.b@intensa.ru>
2021-03-05 14:26:07 +0300

    change file encoding to utf8

--- install/components/mindbox/email.confirm/lang/ru/class.php
+++ install/components/mindbox/email.confirm/lang/ru/class.php
@@ -1,3 +1,3 @@
 <?php
 $MESS['MB_EC_MODULE_NOT_INCLUDED'] = 'Ошибка: модуль #MODULE# не подключен';
-$MESS['MB_EC_BAD_MODULE_SETTING'] = 'Ошибка в настройках модуля;
+$MESS['MB_EC_BAD_MODULE_SETTING'] = 'Ошибка в настройках модуля';
