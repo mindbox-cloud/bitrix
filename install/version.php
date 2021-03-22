368a2dbab5c803699ba403f8acf4ddcedfb5e6dd
maxim.b <maxim.b@intensa.ru>
2021-03-22 11:27:49 +0300

    change version

--- install/version.php
+++ install/version.php
@@ -5,8 +5,8 @@
 
 $arModuleVersion =
     [
-        "VERSION" => "2.2.0",
-        "VERSION_DATE" => "2021-03-10 12:00:00",
+        "VERSION" => "2.3.0",
+        "VERSION_DATE" => "2021-03-22 12:00:00",
         "MODULE_NAME" => GetMessage("MODULE_NAME"),
         "MODULE_DESCRIPTION" => GetMessage("MODULE_DESCRIPTION")
     ];
