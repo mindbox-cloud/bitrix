4380df981eaadc01b6475cce341c9f5acec4bbf8
IvanShishkin <i.shishkin@intensa.ru>
2021-05-19 11:03:23 +0300

    убрал лишнее из файла version.php

--- install/version.php
+++ install/version.php
@@ -1,12 +1,8 @@
 <?php
-/**
- * Created by @copyright QSOFT.
- */
-
 $arModuleVersion =
-    [
-        "VERSION" => "2.3.1",
-        "VERSION_DATE" => "2021-04-20 13:00:00",
-        "MODULE_NAME" => GetMessage("MODULE_NAME"),
-        "MODULE_DESCRIPTION" => GetMessage("MODULE_DESCRIPTION")
-    ];
+[
+    "VERSION" => "2.3.1",
+    "VERSION_DATE" => "2021-04-20 13:00:00",
+    "MODULE_NAME" => GetMessage("MODULE_NAME"),
+    "MODULE_DESCRIPTION" => GetMessage("MODULE_DESCRIPTION")
+];
