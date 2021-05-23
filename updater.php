779df5031faf1fe63371cf9079c38627ce6e95d4
IvanShishkin <i.shishkin@intensa.ru>
2021-05-23 18:30:10 +0300

    добавил проверку возможной версии для обновления.

--- updater.php
+++ updater.php
@@ -1,2 +1,12 @@
 <?
+if (IsModuleInstalled('mindbox.marketing')) {
+    $obj = CModule::CreateModuleObject('mindbox.marketing');
+    $curVersion = $obj->MODULE_VERSION;
+    $allowUpdateVersion = '2.3.0';
+
+    if (CheckVersion($curVersion, $allowUpdateVersion) === false) {
+        echo '<p style="color: red">Ошибка при обновлении модуля. Необходима версия >= 2.3.0</p>';
+        die();
+    }
+}
 
