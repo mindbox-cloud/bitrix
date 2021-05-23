66f147b36bb91a5aab4474d2b054d4a835e1a63b
IvanShishkin <i.shishkin@intensa.ru>
2021-05-23 15:12:32 +0300

    Перенос названия обработчиков в lang файлы

---
+++ lang/ru/lib/EventController.php
@@ -0,0 +1,13 @@
+<?php
+$MESS['OnAfterUserAuthorize'] = 'После авторизации пользователя';
+$MESS['OnBeforeUserRegister'] = 'Перед регистрацией пользователя';
+$MESS['OnAfterUserRegister'] = 'После регистрации пользователя';
+$MESS['OnBeforeUserUpdate'] = 'Перед обновлением пользователя';
+$MESS['OnSaleOrderBeforeSaved'] = 'Перед сохранением заказа';
+$MESS['OnSaleOrderSaved'] = 'После сохранения заказа';
+$MESS['OnSaleBasketSaved'] = 'После сохранения корзины';
+$MESS['OnBeforeSaleOrderFinalAction'] = 'Перед финальным обсчетом';
+$MESS['OnSaleBasketItemRefreshData'] = 'При обновлении корзины';
+$MESS['OnBeforeUserAdd'] = 'Перед добавлением пользователя';
+$MESS['OnAfterUserAdd'] = 'После добавлением пользователя';
+$MESS['OnProlog'] = 'Подключение трекера';
