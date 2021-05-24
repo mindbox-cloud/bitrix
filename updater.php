5e4ec18e218d4e05d4613737965d880b122893b6
IvanShishkin <i.shishkin@intensa.ru>
2021-05-24 20:15:32 +0300

    убрал алерт в updater.php

--- updater.php
+++ updater.php
@@ -1,12 +1,3 @@
 <?
-if (IsModuleInstalled('mindbox.marketing')) {
-    $obj = CModule::CreateModuleObject('mindbox.marketing');
-    $curVersion = $obj->MODULE_VERSION;
-    $allowUpdateVersion = '2.3.0';
 
-    if (CheckVersion($curVersion, $allowUpdateVersion) === false) {
-        echo '<p style="color: red">Ошибка при обновлении модуля. Необходима версия >= 2.3.0</p>';
-        die();
-    }
-}
 
