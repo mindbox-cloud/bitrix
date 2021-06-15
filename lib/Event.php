db9151fb7ff87e805ab35d2809a0151d510aa3ac
maxim.b <maxim.b@intensa.ru>
2021-06-15 11:28:51 +0300

    change payment system name

--- lib/Event.php
+++ lib/Event.php
@@ -690,7 +690,7 @@ class Event
         $paymentCollection = $order->getPaymentCollection();
         foreach ($paymentCollection as $payment) {
             $payments[] = [
-                'type'   => $payment->getPaymentSystemName(),
+                'type'   => $payment->getPaymentSystemId(),
                 'amount' => $payment->getSum()
             ];
         }
