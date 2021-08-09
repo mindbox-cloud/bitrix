884bce2aabaf3f973ec372ffcd8a6cbd69575365
maxim.b <maxim.b@intensa.ru>
2021-08-09 11:09:15 +0300

    fix syntax

--- install/components/mindbox/order.history/templates/.default/template.php
+++ install/components/mindbox/order.history/templates/.default/template.php
@@ -2,7 +2,7 @@
 /**
  * Created by @copyright QSOFT.
  */
-if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) {
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
     die();
 }
 
@@ -16,55 +16,54 @@ Asset::getInstance()->addCss("/bitrix/css/mindbox/style.css");
 ?>
 
 <div class="mindbox">
-    <?php
-    if (!empty($arResult['ERROR'])) :
-        ?>
-        <span><?=$arResult['ERROR']?></span>
-        <?php
-    else :
-        ?>
-        <?php
-        if (empty($arResult['ORDERS'])) :
-            ?>
-            <span><?=GetMessage('MB_OH_EMPTY_MESSAGE')?></span>
-        <?php endif; ?>
+    <? if (!empty($arResult['ERROR'])): ?>
+        <span><?= $arResult['ERROR'] ?></span>
+    <? else: ?>
+        <? if (empty($arResult['ORDERS'])): ?>
+            <span><?= GetMessage('MB_OH_EMPTY_MESSAGE') ?></span>
+        <? endif; ?>
         <div id='mindbox-orders-history'>
             <?php foreach ($arResult['ORDERS'] as $order) : ?>
-                <?=GetMessage('MB_OH_ORDER_HEADER', ['#ID#' => $order['id'], '#CREATED#' => $order['created'] ])?>
+                <?= GetMessage('MB_OH_ORDER_HEADER', ['#ID#' => $order['id'], '#CREATED#' => $order['created']]) ?>
 
                 <p>
                     <?php if ($order['spentBonuses']) : ?>
-                        <?=GetMessage('MB_OH_ORDER_SPENT', ['#SPENT#' => $order['spentBonuses'], '#END#' => Helper::getNumEnding(
-                            $order['spentBonuses'],
-                            GetMessage('MB_OH_ENDINGS_ARRAY')
-                        )]);?>
+                        <?= GetMessage('MB_OH_ORDER_SPENT', [
+                            '#SPENT#' => $order['spentBonuses'],
+                            '#END#'   => Helper::getNumEnding(
+                                $order['spentBonuses'],
+                                GetMessage('MB_OH_ENDINGS_ARRAY')
+                            )
+                        ]); ?>
                     <?php endif; ?>
                     <?php if ($order['acuiredBonuses']) : ?>
-                         <?=GetMessage('MB_OH_ORDER_ACUIRED', ['#ACUIRED#' => $order['acuiredBonuses'],'#END#' => Helper::getNumEnding(
-                             $order['acuiredBonuses'],
-                             GetMessage('MB_OH_ENDINGS_ARRAY')
-                         )]);?>
+                        <?= GetMessage('MB_OH_ORDER_ACUIRED', [
+                            '#ACUIRED#' => $order['acuiredBonuses'],
+                            '#END#'     => Helper::getNumEnding(
+                                $order['acuiredBonuses'],
+                                GetMessage('MB_OH_ENDINGS_ARRAY')
+                            )
+                        ]); ?>
                     <?php endif; ?>
                 </p>
 
                 <table class="table table-bordered table-striped">
                     <tr>
-                        <th><?=GetMessage('MB_OH_ITEM_NAME')?></th>
-                        <th><?=GetMessage('MB_OH_ITEM_PRICE')?></th>
+                        <th><?= GetMessage('MB_OH_ITEM_NAME') ?></th>
+                        <th><?= GetMessage('MB_OH_ITEM_PRICE') ?></th>
                     </tr>
                     <?php foreach ($order['lines'] as $line) : ?>
-                        <?=GetMessage('MB_OH_ORDER_LINE', ['#LINK#' => $line['link'], '#NAME#' => $line['name'], '#PRICE#' => $line['price']]);?>
+                        <?= GetMessage('MB_OH_ORDER_LINE',
+                            ['#LINK#' => $line['link'], '#NAME#' => $line['name'], '#PRICE#' => $line['price']]); ?>
                     <?php endforeach; ?>
                 </table>
             <?php endforeach; ?>
         </div>
         <?php if (count($arResult['ORDERS']) === $arParams['PAGE_SIZE']) : ?>
-        <div class="more" id="mindbox-order-more" data-page="1">
-            <div class="btn btn-primary" id="mindbox-order-history--load-more"><?=GetMessage('MB_OH_MORE')?></div>
-        </div>
+            <div class="more" id="mindbox-order-more" data-page="1">
+                <div class="btn btn-primary" id="mindbox-order-history--load-more"><?= GetMessage('MB_OH_MORE') ?></div>
+            </div>
         <?php endif; ?>
 
-        <?php
-    endif;
-    ?>
+    <? endif; ?>
 </div>
