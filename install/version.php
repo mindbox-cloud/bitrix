110f28ddf5457fbb9efba15fdf82c7d3b0bf0f0b
IvanShishkin <i.shishkin@intensa.ru>
2021-08-10 11:10:27 +0300

    вернул версию

--- install/version.php
+++ install/version.php
@@ -1,6 +1,6 @@
 <?
 $arModuleVersion = array(
-	"VERSION" => "2.4.2",
+	"VERSION" => "2.4.0",
 	"VERSION_DATE" => "2021-05-19 11:05:00",
     "MODULE_NAME" => GetMessage("MODULE_NAME"),
     "MODULE_DESCRIPTION" => GetMessage("MODULE_DESCRIPTION")
