4d190e3628622a9b500312dd1f65731c0af1225c
maxim.b <maxim.b@intensa.ru>
2021-08-18 13:28:51 +0300

    change version date

--- install/version.php
+++ install/version.php
@@ -1,8 +1,7 @@
-<?
+<?php
 $arModuleVersion = array(
-	"VERSION" => "2.4.2",
-	"VERSION_DATE" => "2021-08-18 13:30:00",
+    "VERSION" => "2.4.2",
+    "VERSION_DATE" => "2021-08-18 13:30:00",
     "MODULE_NAME" => GetMessage("MODULE_NAME"),
     "MODULE_DESCRIPTION" => GetMessage("MODULE_DESCRIPTION")
 );
-?>
