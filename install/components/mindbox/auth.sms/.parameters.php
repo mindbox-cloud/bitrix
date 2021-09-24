d939a8efa12448239a416cdbf0ce51131a9e8abc
IvanShishkin <i.shishkin@intensa.ru>
2021-09-24 19:51:38 +0300

    Фикс ключа в параметрах компонента auth.sms

--- install/components/mindbox/auth.sms/.parameters.php
+++ install/components/mindbox/auth.sms/.parameters.php
@@ -1,13 +1,13 @@
 <?php
-if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true) die();
 
 $arComponentParameters = [
-    "PARAMETERS" => [
-        "USE_BONUSES" => [
-            "PARENT" => "BASE",
-            "NAME" => GetMessage('PERSONAL_PAGE_URL'),
-            "TYPE" => "STRING",
-            "DEFAULT" => SITE_DIR . '/'
+    'PARAMETERS' => [
+        'PERSONAL_PAGE_URL' => [
+            'PARENT' => 'BASE',
+            'NAME' => GetMessage('PERSONAL_PAGE_URL'),
+            'TYPE' => 'STRING',
+            'DEFAULT' => SITE_DIR . '/'
         ]
     ]
 ];
