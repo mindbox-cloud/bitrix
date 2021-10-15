2aacb5a6f74c5cc044063d0b219da32e5dea4748
maxim.b <maxim.b@intensa.ru>
2021-10-15 14:51:22 +0300

    В payments нужно передавать без amount, только type

--- lib/Event.php
+++ lib/Event.php
@@ -1538,8 +1538,7 @@ class Event
         $paymentCollection = $order->getPaymentCollection();
         foreach ($paymentCollection as $payment) {
             $payments[] = [
-                'type'   => $payment->getPaymentSystemId(),
-                'amount' => $payment->getSum()
+                'type'   => $payment->getPaymentSystemId()
             ];
         }
 
