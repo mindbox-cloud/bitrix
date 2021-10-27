6c799f3fe073f6e5a0f39007ecaac5314912d5c6
IvanShishkin <i.shishkin@intensa.ru>
2021-10-27 15:01:36 +0300

    Доработка updater.php

--- updater.php
+++ updater.php
@@ -2,9 +2,18 @@
 
 if(IsModuleInstalled('mindbox.marketing'))
 {
+    \CModule::IncludeModule('mindbox.marketing');
+
     if (is_dir(dirname(__FILE__).'/install/components'))
         $updater->CopyFiles("install/components", "components/");
 
-    $objEventController = new \Mindbox\EventController();
-    $objEventController->installEvents();
+    $eventManager = \Bitrix\Main\EventManager::getInstance();
+    $eventManager->registerEventHandler(
+        'main',
+        'OnBeforeProlog',
+        'mindbox.marketing',
+        '\Mindbox\Event',
+        'OnBeforePrologHandler',
+        1000
+    );
 }
