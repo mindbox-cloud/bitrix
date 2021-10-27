ae160c96c5a7159ffaaf40c079ec93a40c17cda3
IvanShishkin <i.shishkin@intensa.ru>
2021-10-27 14:10:23 +0300

    Изменил файл updater.php

--- updater.php
+++ updater.php
@@ -1,1 +1,10 @@
 <?php
+
+if(IsModuleInstalled('mindbox.marketing'))
+{
+    if (is_dir(dirname(__FILE__).'/install/components'))
+        $updater->CopyFiles("install/components", "components/");
+
+    $objEventController = new \Mindbox\EventController();
+    $objEventController->installEvents();
+}
