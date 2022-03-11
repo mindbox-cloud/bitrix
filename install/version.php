ba48d925071ed772e787f6c9eab773cc2ec144d7
IvanShishkin <i.shishkin@intensa.ru>
2022-03-11 15:19:49 +0300

    Исправление бага с дублированием в списке модулей

--- install/version.php
+++ install/version.php
@@ -2,6 +2,6 @@
 $arModuleVersion = array(
     "VERSION" => "2.6.0",
     "VERSION_DATE" => "2021-01-28 15:00:00",
-    "MODULE_NAME" => GetMessage("MINDBOX_MODULE_NAME"),
+    "MODULE_NAME" => GetMessage("MODULE_NAME"),
     "MODULE_DESCRIPTION" => GetMessage("MODULE_DESCRIPTION")
 );
