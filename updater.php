422c260c3e195d453d3ebd79a8d98be553d34e96
IvanShishkin <i.shishkin@intensa.ru>
2022-03-14 12:03:49 +0300

    Добавли в updater.php скрипт для удаления устаревших событий из базы

--- updater.php
+++ updater.php
@@ -3,4 +3,18 @@
 if (IsModuleInstalled('mindbox.marketing')) {
     \CModule::IncludeModule('mindbox.marketing');
 
+
+    $eventController = new \Mindbox\EventController();
+    $eventController->unRegisterEventHandler([
+        'bitrixModule' => 'main',
+        'bitrixEvent' => 'OnBeforeUserRegister',
+        'class' => '\Mindbox\Event',
+        'method' => 'OnBeforeUserRegisterHandler',
+    ]);
+    $eventController->unRegisterEventHandler([
+        'bitrixModule' => 'main',
+        'bitrixEvent' => 'OnAfterUserRegister',
+        'class' => '\Mindbox\Event',
+        'method' => 'OnAfterUserRegisterHandler',
+    ]);
 }
