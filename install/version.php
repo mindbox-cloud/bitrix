ca3bb7e94c1097164fb897adac5c806c84bd8017
IvanShishkin <i.shishkin@intensa.ru>
2022-03-14 11:46:10 +0300

    Изменил версию модуля. Изменил файл updater для текущей версии

--- install/version.php
+++ install/version.php
@@ -1,7 +1,7 @@
 <?php
 $arModuleVersion = array(
-    "VERSION" => "2.6.0",
-    "VERSION_DATE" => "2021-01-28 15:00:00",
+    "VERSION" => "2.6.3",
+    "VERSION_DATE" => "2021-03-11 15:00:00",
     "MODULE_NAME" => GetMessage("MINDBOX_MODULE_NAME"),
     "MODULE_DESCRIPTION" => GetMessage("MODULE_DESCRIPTION")
 );
--- updater.php
+++ updater.php
@@ -3,19 +3,4 @@
 if (IsModuleInstalled('mindbox.marketing')) {
     \CModule::IncludeModule('mindbox.marketing');
 
-    if (is_dir(dirname(__FILE__).'/install/components')) {
-        $updater->CopyFiles("install/components", "components/");
-    }
-
-    $updater->CopyFiles("lib", "modules/mindbox.marketing/lib");
-
-    try {
-        (new \Mindbox\EventController())->installDeliveryRulesHandler();
-    } catch (\Exception $e) {
-    }
-
-    try {
-        (new \Mindbox\Installer\DeliveryCartRuleInstaller())->install();
-    } catch (\Exception $e) {
-    }
 }
