1606747b066eb454bfc3b6ee34ff375d25a4d31e
IvanShishkin <i.shishkin@intensa.ru>
2022-03-14 11:54:44 +0300

    Исправления версии

--- install/version.php
+++ install/version.php
@@ -1,6 +1,6 @@
 <?php
 $arModuleVersion = array(
-    "VERSION" => "2.6.3",
+    "VERSION" => "2.6.2",
     "VERSION_DATE" => "2021-03-11 15:00:00",
     "MODULE_NAME" => GetMessage("MINDBOX_MODULE_NAME"),
     "MODULE_DESCRIPTION" => GetMessage("MODULE_DESCRIPTION")
