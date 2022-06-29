de6a2bd2a54aa2fd3bdc7a33d648b10738df7714
maxim.b <maxim.b@intensa.ru>
2022-06-29 12:31:40 +0300

    Для дозаполнения профиля проверять в майндбоксе наличие пользователя с таким же email.

--- install/components/mindbox/auth.sms/templates/.default/script.js
+++ install/components/mindbox/auth.sms/templates/.default/script.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
-    var mindboxValidationMap = {
+    var phone = localStorage.getItem('phone');
+    var $error = $('#mindbox-auth-sms-error');
+    var $success = $('#mindbox-auth-sms-success');
+
+    var validationMap = {
         '/customer/birthDate': $('#mindbox-fillup-birth-date-error'),
         '/customer/mobilePhone': $('#mindbox-fillup-phone-error'),
         '/customer/lastName': $('#mindbox-fillup-last-name-error'),
@@ -7,28 +11,33 @@ $(document).ready(function () {
         '/customer/email': $('#mindbox-fillup-email-error'),
     };
 
-    var mindboxCaptchaReload = function () {
-        let request = BX.ajax.runComponentAction('mindbox:auth.sms', 'captchaUpdate', {
+    var inputCode = function (phone) {
+        $('#mindbox-input-phone').hide();
+        $('#mindbox-auth-sms--mobile-phone').text(phone);
+        $('#mindbox-input-code').show();
+    };
+
+    var captchaReload = function () {
+        var request = BX.ajax.runComponentAction('mindbox:auth.sms', 'captchaUpdate', {
             mode: 'class',
         });
 
         request.then(function (response) {
-            $('#mindbox---captcha_sid').val(response.data.captcha_sid);
-            $('#mindbox--captcha_img').attr("src", "/bitrix/tools/captcha.php?captcha_sid=" + response.data.captcha_sid);
+            var data = response.data;
+            $('#mindbox---captcha_sid').val(data.captcha_sid);
+            $('#mindbox--captcha_img').attr("src", "/bitrix/tools/captcha.php?captcha_sid=" + data.captcha_sid);
         });
     };
 
     $('#mindbox-input-phone').on('submit', function (event) {
-        let $targetForLoader = $('#mindbox-input-phone');
+        var $targetForLoader = $('#mindbox-input-phone');
 
         loader.show($targetForLoader);
         event.preventDefault();
 
-        let phone = $('#mindbox-num').val();
-
+        phone = $('#mindbox-num').val();
         localStorage.setItem('phone', phone);
-
-        let request = BX.ajax.runComponentAction('mindbox:auth.sms', 'sendCode', {
+        var request = BX.ajax.runComponentAction('mindbox:auth.sms', 'sendCode', {
             mode: 'class',
             data: {
                 phone: phone
@@ -39,18 +48,17 @@ $(document).ready(function () {
             loader.hide($targetForLoader);
 
             if (response.data.type === 'success') {
-                $('#mindbox-auth-sms-error').hide();
-                $('#mindbox-input-phone').hide();
-                $('#mindbox-auth-sms--mobile-phone').text(phone);
-                $('#mindbox-input-code').show();
+                $error.hide();
+                inputCode(phone);
             } else if (response.data.type === 'error') {
-                $('#mindbox-auth-sms-error').text(response.data.message).show();
+                $error.text(response.data.message);
+                $error.show();
             }
         });
     });
 
     $('#mindbox-submit-code--reset').on('click', function () {
-        $('#mindbox-auth-sms-error').hide();
+        $error.hide();
         localStorage.setItem('phone', '');
         $('#mindbox-input-phone').show();
 
@@ -58,11 +66,8 @@ $(document).ready(function () {
     });
 
     $('#mindbox-submit-code--resend').on('click', function () {
-        let phone = localStorage.getItem('phone');
-
-        $('#mindbox-auth-sms-error').hide();
-
-        let request = BX.ajax.runComponentAction('mindbox:auth.sms', 'resend', {
+        $error.hide();
+        var request = BX.ajax.runComponentAction('mindbox:auth.sms', 'resend', {
             mode: 'class',
             data: {
                 phone: phone
@@ -71,16 +76,14 @@ $(document).ready(function () {
     });
 
     $('#mindbox-input-code').on('submit', function (event) {
-        event.preventDefault();
-
-        let phone = localStorage.getItem('phone');
-        let $targetForLoader = $('#mindbox-input-code');
+        var $targetForLoader = $('#mindbox-input-code');
 
         loader.show($targetForLoader);
+        event.preventDefault();
 
-        let code = $('#mindbox-code').val();
+        var code = $('#mindbox-code').val();
 
-        let request = BX.ajax.runComponentAction('mindbox:auth.sms', 'checkCode', {
+        var request = BX.ajax.runComponentAction('mindbox:auth.sms', 'checkCode', {
             mode: 'class',
             data: {
                 code: code,
@@ -95,52 +98,44 @@ $(document).ready(function () {
                 localStorage.setItem('phone', '');
                 window.location.reload()
             } else if (response.data.type === 'error') {
-                $('#mindbox-auth-sms-error').text(response.data.message).show();
+                $error.text(response.data.message);
+                $error.show();
             } else if (response.data.type === 'fillup') {
 
-                if ($('#mindbox-fillup-profile').length) {
-                    let $genderId = (response.data.sex === 'male') ? 1 : 2;
+                let $genderId = (response.data.sex === 'male') ? 1 : 2;
 
-                    $('#mindbox-input-code').hide();
-                    $('#mindbox-fillup-profile').show();
+                $('#mindbox-input-code').hide();
+                $('#mindbox-fillup-profile').show();
 
-                    $('#mindbox-fillup-phone').val(response.data.mobilePhone);
-                    $('#mindbox-fillup-email').val(response.data.email);
-                    $('#mindbox-fillup-name').val(response.data.firstName);
-                    $('#mindbox-fillup-last-name').val(response.data.lastName);
-                    $('#mindbox-fillup-date').val(response.data.birthDate);
-                    $('#mindbox-gender_' + $genderId).attr('checked', 'checked');
-                } else {
-                    localStorage.setItem('phone', '');
-                    window.location.reload()
-                }
+                $('#mindbox-fillup-phone').val(response.data.mobilePhone);
+                $('#mindbox-fillup-email').val(response.data.email);
+                $('#mindbox-fillup-email-original').val(response.data.email);
+                $('#mindbox-fillup-name').val(response.data.firstName);
+                $('#mindbox-fillup-last-name').val(response.data.lastName);
+                $('#mindbox-fillup-date').val(response.data.birthDate);
+                $('#mindbox-gender_' + $genderId).attr('checked', 'checked');
+
             }
         });
     });
 
     $('#mindbox-fillup-profile').on('submit', function (e) {
         e.preventDefault();
-        let $targetForLoader = $(this);
+        var $targetForLoader = $(this);
         loader.show($targetForLoader);
 
-        let fields = {
-            'NAME': $('#mindbox-fillup-name').val(),
-            'EMAIL': $('#mindbox-fillup-email').val(),
-            'LAST_NAME': $('#mindbox-fillup-last-name').val(),
-            'PERSONAL_PHONE': $('#mindbox-fillup-phone').val(),
-            'PERSONAL_BIRTHDAY': $('#mindbox-fillup-date').val(),
-            'PASSWORD': $('#mindbox-fillup-password').val(),
-            'CONFIRM_PASSWORD': $('#mindbox-fillup-password').val(),
-            'PERSONAL_GENDER': $("input[name='mindbox-fillup-gender']:checked").val()
-        };
+        var fields = [];
 
-        for (let fieldKey in fields) {
-            if (fields[fieldKey] === null || fields[fieldKey] === undefined) {
-                delete fields[fieldKey];
-            }
-        }
-
-        if ($('#mindbox--captcha_sid').val() !== undefined && $('#mindbox--captcha_word').val() !== undefined) {
+        fields['NAME'] = $('#mindbox-fillup-name').val();
+        fields['EMAIL'] = $('#mindbox-fillup-email').val();
+        fields['EMAIL_ORIGINAL'] = $('#mindbox-fillup-email-original').val();
+        fields['LAST_NAME'] = $('#mindbox-fillup-last-name').val();
+        fields['PERSONAL_PHONE'] = $('#mindbox-fillup-phone').val();
+        fields['PERSONAL_BIRTHDAY'] = $('#mindbox-fillup-date').val();
+        fields['PASSWORD'] = $('#mindbox-fillup-password').val();
+        fields['CONFIRM_PASSWORD'] = $('#mindbox-fillup-password').val();
+        fields['PERSONAL_GENDER'] = $("input[name='mindbox-fillup-gender']:checked").val();
+        if ($('#mindbox--captcha_sid').val() != undefined && $('#mindbox--captcha_word').val() != undefined) {
             fields['captcha_word'] = $('#mindbox--captcha_word').val();
             fields['captcha_sid'] = $('#mindbox--captcha_sid').val();
         } else {
@@ -148,30 +143,33 @@ $(document).ready(function () {
             fields['captcha_sid'] = 0;
         }
 
-        let request = BX.ajax.runComponentAction('mindbox:auth.sms', 'fillup', {
+
+        var request = BX.ajax.runComponentAction('mindbox:auth.sms', 'fillup', {
             mode: 'class',
             data: {
                 fields: fields
             }
         });
 
+
         request.then(function (response) {
             loader.hide($targetForLoader);
+            var data = response.data;
 
-            if (response.data.type === 'error') {
-                $('#mindbox-auth-sms-error').html(response.data.message).show();
+            if (data.type === 'error') {
+                $error.html(data.message);
+                $error.show();
 
-                mindboxCaptchaReload();
-            } else if (response.data.type === 'validation errors') {
-                for (let error of response.data.errors) {
-                    let $field = mindboxValidationMap[error.location];
-
+                captchaReload();
+            } else if (data.type === 'validation errors') {
+                for (var error of data.errors) {
+                    var $field = validationMap[error.location];
                     $field.text(error.message);
                     $field.closest($('.form-group')).addClass('has-error');
                 }
 
-                mindboxCaptchaReload();
-            } else if (response.data.type === 'success') {
+                captchaReload();
+            } else if (data.type === 'success') {
                 window.location.reload()
             }
         });
